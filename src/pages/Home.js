6850f94e9b8e19058e655481035f6c952d259ceb
Guillaume Levergeois <120370273+glevergeois@users.noreply.github.com>
2023-12-06 14:30:48 +0100

    filterpokemonlist

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,62 +1,52 @@
 import { useContext, useRef, useEffect } from 'react';
-import '../css/home.css'
-// import GlareCard from '../components/Card'
-import Card from '../components/Card'
-import {FilterContext} from '../contexts/FilterContext';
+import '../css/home.css';
+import Card from '../components/Card';
+import { FilterContext } from '../contexts/FilterContext';
 import { OpenCardContext } from '../contexts/OpenCardContext';
 import $ from 'jquery';
-
-
+import filterpokemonlist from '../tools/filterpokemonlist';
 
 export default function Home() {
+  const { pokemonList, filterValue } = useContext(FilterContext);
+  const { openCardValue, closeCardfunction } = useContext(OpenCardContext);
 
-    const { pokemonList, filterValue} = useContext(FilterContext)
+  useEffect(() => {
+    if (openCardValue === 'open') {
+      backgroundBlur('on');
+      console.log('on');
+    } else if (openCardValue === 'close') {
+      backgroundBlur('off');
+      console.log('off');
+    }
+  }, [openCardValue]);
 
-    const { openCardValue , closeCardfunction} = useContext(OpenCardContext);
+  const blurry_background = useRef(null);
 
-    useEffect(() => {
-        if (openCardValue === 'open'){
-            backgroundBlur('on')
-            console.log('on')
-        }
-        else if (openCardValue === 'close'){
-            backgroundBlur('off')
-            console.log('off')
-        }
-    }, [openCardValue]); 
-
-
-    useEffect(() => {
-        console.log(filterValue)
-      }, [filterValue])
-
-    const blurry_background = useRef(null)
-   
-    function backgroundBlur(x){
-        const blurry_background_element = blurry_background.current;
-        if (x === 'on'){
-            $(blurry_background_element).css('display', 'initial')
-        }else if (x === 'off'){
-            $(blurry_background_element).css('display', 'none')
-        }
-   }
-    
-
-    var language= 'fr'
-
+  useEffect(() => {
+    const filteredPokemonList = filterpokemonlist(pokemonList, filterValue);
+    var language = 'fr';
     return (
-        <>
-            <div className="home">
-                {pokemonList.map(pokemon => (
-                    <Card key={pokemon.id} language={language} pokemon={pokemon}  />
-                ))}
-            </div>
-            <div ref={blurry_background} onClick={closeCardfunction} className='background-blur'></div>
-        </>
+      <>
+        <div className="home">
+          {filteredPokemonList.map((pokemon) => (
+            <Card key={pokemon.id} language={language} pokemon={pokemon} />
+          ))}
+        </div>
+        <div
+          ref={blurry_background}
+          onClick={closeCardfunction}
+          className="background-blur"
+        ></div>
+      </>
     );
+  }, [filterValue]);
+
+  function backgroundBlur(x) {
+    const blurry_background_element = blurry_background.current;
+    if (x === 'on') {
+      $(blurry_background_element).css('display', 'initial');
+    } else if (x === 'off') {
+      $(blurry_background_element).css('display', 'none');
+    }
+  }
 }
-
-
-
-
-
