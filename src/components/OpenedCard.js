8093e6cbd2057500ea1dab913cde8f18bd5ba1be
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2023-12-15 13:54:58 +0100

    oui

--- src/components/OpenedCard.js
+++ src/components/OpenedCard.js
@@ -6,27 +6,190 @@ import { OpenCardContext } from '../contexts/OpenCardContext';
 import $ from 'jquery';
 import int_to_hashtag from '../tools/int_to_hashtag'
 import ApexCharts from 'apexcharts'; 
+import '../css/card.css'
+import fond_image_pokemon from '../images/fond-image-pokemon.svg'
+
+
+
+// images front 
+import fond_card_1 from "../images/fond-card/fond-carte-squircle-1.svg";
+import fond_card_2 from "../images/fond-card/fond-carte-squircle-2.svg";
+import fond_card_3 from "../images/fond-card/fond-carte-squircle-3.svg";
+import fond_card_4 from "../images/fond-card/fond-carte-squircle-4.svg";
+import fond_card_5 from "../images/fond-card/fond-carte-squircle-5.svg";
+import fond_card_6 from "../images/fond-card/fond-carte-squircle-6.svg";
+import fond_card_7 from "../images/fond-card/fond-carte-squircle-7.svg";
+import fond_card_8 from "../images/fond-card/fond-carte-squircle-8.svg";
+import fond_card_9 from "../images/fond-card/fond-carte-squircle-9.svg";
+import fond_card_10 from "../images/fond-card/fond-carte-squircle-10.svg";
+import fond_card_11 from "../images/fond-card/fond-carte-squircle-11.svg";
+import fond_card_12 from "../images/fond-card/fond-carte-squircle-12.svg";
+import fond_card_13 from "../images/fond-card/fond-carte-squircle-13.svg";
+import fond_card_14 from "../images/fond-card/fond-carte-squircle-14.svg";
+import fond_card_15 from "../images/fond-card/fond-carte-squircle-15.svg";
+import fond_card_16 from "../images/fond-card/fond-carte-squircle-16.svg";
+import fond_card_17 from "../images/fond-card/fond-carte-squircle-17.svg";
+import fond_card_18 from "../images/fond-card/fond-carte-squircle-18.svg";
+
+
+// images mask 
+import fond_card_mask_1 from "../images/fond-card-mask/fond-card-mask-1.svg";
+import fond_card_mask_2 from "../images/fond-card-mask/fond-card-mask-2.svg";
+import fond_card_mask_3 from "../images/fond-card-mask/fond-card-mask-3.svg";
+import fond_card_mask_4 from "../images/fond-card-mask/fond-card-mask-4.svg";
+import fond_card_mask_5 from "../images/fond-card-mask/fond-card-mask-5.svg";
+import fond_card_mask_6 from "../images/fond-card-mask/fond-card-mask-6.svg";
+import fond_card_mask_7 from "../images/fond-card-mask/fond-card-mask-7.svg";
+import fond_card_mask_8 from "../images/fond-card-mask/fond-card-mask-8.svg";
+import fond_card_mask_9 from "../images/fond-card-mask/fond-card-mask-9.svg";
+import fond_card_mask_10 from "../images/fond-card-mask/fond-card-mask-10.svg";
+import fond_card_mask_11 from "../images/fond-card-mask/fond-card-mask-11.svg";
+import fond_card_mask_12 from "../images/fond-card-mask/fond-card-mask-12.svg";
+import fond_card_mask_13 from "../images/fond-card-mask/fond-card-mask-13.svg";
+import fond_card_mask_14 from "../images/fond-card-mask/fond-card-mask-14.svg";
+import fond_card_mask_15 from "../images/fond-card-mask/fond-card-mask-15.svg";
+import fond_card_mask_16 from "../images/fond-card-mask/fond-card-mask-16.svg";
+import fond_card_mask_17 from "../images/fond-card-mask/fond-card-mask-17.svg";
+import fond_card_mask_18 from "../images/fond-card-mask/fond-card-mask-18.svg";
+
+
+// images back 
+import back_card from "../images/back-card-squircle.svg";
+
+
+
+
 
 export default function Opened_card(props) {
 
     const { typeList } = useContext(PokemonContext)
     const pokemon = props.pokemon;
 
-    console.log(pokemon['id'])
+    let language= 'fr'
+    var name = pokemon['name'][language].normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase()
+    var pk_number = int_to_hashtag(pokemon['id'])
+
+    var pokemon_type = pokemon['types']
+
+    var pokemon_type_name = []
+    var pokemon_type_url = []
 
     function handleCardClose() {
     props.onClose();
     }
+
+
+
+    // images 
+    const fondCardImages = [
+      fond_card_1,
+      fond_card_2,
+      fond_card_3,
+      fond_card_4,
+      fond_card_5,
+      fond_card_6,
+      fond_card_7,
+      fond_card_8,
+      fond_card_9,
+      fond_card_10,
+      fond_card_11,
+      fond_card_12,
+      fond_card_13,
+      fond_card_14,
+      fond_card_15,
+      fond_card_16,
+      fond_card_17,
+      fond_card_18,
+    ];
+    // uniquement le 1er
+    const fond_card_svg = fondCardImages[pokemon_type[0] - 1];
+
+    const fondCardImagesMask = [
+      fond_card_mask_1,
+      fond_card_mask_2,
+      fond_card_mask_3,
+      fond_card_mask_4,
+      fond_card_mask_5,
+      fond_card_mask_6,
+      fond_card_mask_7,
+      fond_card_mask_8,
+      fond_card_mask_9,
+      fond_card_mask_10,
+      fond_card_mask_11,
+      fond_card_mask_12,
+      fond_card_mask_13,
+      fond_card_mask_14,
+      fond_card_mask_15,
+      fond_card_mask_16,
+      fond_card_mask_17,
+      fond_card_mask_18,
+    ];
+    // uniquement le 1er
+
+
+
+
+    // afficher les 
+    pokemon_type.forEach(pokemonType => {
+      typeList.forEach(element => {
+          if (element['id'] === parseInt(pokemonType, 10)) {
+              pokemon_type_name.push(element['name'][language]);
+              pokemon_type_url.push(element['image']);
+              // console.log(pokemon_type_name)
+          }
+      });
+    });
+
+
+    // const [cardState, setCardState] = useState('front');
+
+    const [tiltProps, setTiltProps] = useState({
+      scale: 1.15,
+      glareEnable: true,
+      glareMaxOpacity: 0.8,
+      glareColor: "#ffffff",
+      glarePosition: "bottom",
+      glareBorderRadius: "20px",
+      trackOnWindow: false,
+    });
+
+    const back_ = useRef(null)
+    const big_front = useRef(null)
+    const card_element = useRef(null)
+    const paralax_element = useRef(null)
+    // const paralax_element_2 = useRef(null)
+    const big_front_container_element = useRef(null)
+
+
+    function render_BG_Images() {
+      const pokemon_type_length = pokemon_type.length;
+
+      if (pokemon_type_length === 2) {
+          const fond_card_mask_svg = fondCardImagesMask[pokemon_type[1] - 1];
+          return (
+              <>
+                  <img className='fond-carte' src={fond_card_svg} alt="carte" />
+                  <img className='fond-carte-test' src={fond_card_mask_svg} alt="carte" />
+              </>
+          );
+      } else {
+          return (
+              <img className='fond-carte' src={fond_card_svg} alt="carte" />
+          );
+      }
+    }
+
+    var pokemon_generation_big = "GENERATION " + pokemon['generation']
   
     return (
-      <div>
-        <div class="opencard">
-            {pokemon['name']['fr']}
-        </div>
-        <button onClick={handleCardClose}>Fermer la carte</button>
+      <div ref={card_element} id={'pokemon'+pokemon['id']} className='card_wraper'>
+      <Tilt ref={paralax_element} className="parallax-effect-glare-scale" {...tiltProps}>
+      </Tilt>
       </div>
     );
+
   }
+  // <button onClick={handleCardClose}>Fermer la carte</button>
 
   
   // var { language, pokemon } = props
