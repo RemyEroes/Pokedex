439c94449c2037543eb653a39b913f2b5ddb2bf2
RemyEroes <113907739+RemyEroes@users.noreply.github.com>
2023-12-15 10:22:28 +0100

    curseur qui rotate

--- src/components/Pointer.js
+++ src/components/Pointer.js
@@ -5,24 +5,53 @@ import $ from 'jquery';
 
 export default function Pointer() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [rotation, setRotation] = useState(0);
 
   useEffect(() => {
     const updatePosition = (e) => {
       setPosition({ x: e.clientX, y: e.clientY });
     };
 
+    let lastMouseX = 0;
+    let lastMouseY = 0;
+
+    // const updateRotation = (e) => {
+    //   let mouseX = e.pageX;
+    //   let mouseY = e.pageY;
+    //   let deplacementX = mouseX - lastMouseX;
+    //   let deplacementY = mouseY - lastMouseY;
+    //   let angleDeg = (deplacementX * 40) / 20;
+    //   lastMouseX = mouseX;
+    //   setRotation(angleDeg);
+    // };
+    const updateRotation = (e) => {
+        let mouseX = e.pageX;
+        let mouseY = e.pageY;
+        let deplacementX = mouseX - lastMouseX;
+        let deplacementY = mouseY - lastMouseY;
+        // let angleDeg = (deplacementX * 40 + deplacementY * 40) / 20;
+        let angleDeg = (deplacementX * 40 + deplacementY * 40) / 30;
+        lastMouseX = mouseX;
+        lastMouseY = mouseY;
+        setRotation(angleDeg);
+      };
+
+  
+
     $(document).on('mousemove', updatePosition);
+    $(document).on('mousemove', updateRotation);
 
     return () => {
       $(document).off('mousemove', updatePosition);
+      $(document).off('mousemove', updateRotation);
     };
-  }, []); // useEffect ne déclenchera cet effet qu'une seule fois lors du montage du composant
+  }, []);
 
   const cursorStyle = {
     position: 'fixed',
     top: `${position.y}px`,
     left: `${position.x}px`,
-    transform: 'translate(-50%, -50%)',
+    transform: `translate(-50%, -50%) rotate(${rotation}deg)`, // Utilisez des backticks pour les template strings
     zIndex: 9999,
   };
 
@@ -32,5 +61,4 @@ export default function Pointer() {
     </div>
   );
 }
-
 
