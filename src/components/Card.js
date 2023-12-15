0ea02fe9a33dc1118e365cbce1f249f93abf90b8
Guillaume Levergeois <120370273+glevergeois@users.noreply.github.com>
2023-12-15 09:48:01 +0100

    radar

--- src/components/Card.js
+++ src/components/Card.js
@@ -306,3 +306,63 @@ export default function Card(props) {
 
 }
  // <CloseButton onClick={() => closeCard()} />
+
+
+
+
+
+var options = {
+    series: [{
+    name: 'Series 1',
+    data: [20, 100, 40, 30, 50, 80, 33, 45],
+    }],
+    chart: {
+    height: 350,
+    type: 'radar',
+    },
+    //   dataLabels: {
+    //     enabled: true
+    //   },
+    plotOptions: {
+    radar: {
+        size: 140,
+        polygons: {
+        strokeColors: '#e9e9e9',
+        fill: {
+            colors: ['#f8f8f8', '#fff']
+        }
+        }
+    }
+    },
+    title: {
+    text: 'Radar with Polygon Fill'
+    },
+    colors: ['#FF4560'],
+    markers: {
+    size: 4,
+    colors: ['#fff'],
+    strokeColor: '#FF4560',
+    strokeWidth: 2,
+    },
+    tooltip: {
+    y: {
+        formatter: function(val) {
+        return val
+        }
+    }
+    },
+    xaxis: {
+    categories: ['Attaque : ', 'Attaque spéciale : ', 'PV : ', 'Defense : ', 'Defense spéciale : ', 'Vitesse : ', 'Taille : ', 'Poids : ']
+    },
+    yaxis: {
+    tickAmount: 8,
+    labels: {
+        formatter: function(val, i) {
+        return ''
+        }
+    }
+    }
+    };
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
