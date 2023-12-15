72d208cc9141b5eb8dcd371b79cffe50b10e113d
Guillaume Levergeois <120370273+glevergeois@users.noreply.github.com>
2023-12-15 13:58:17 +0100

    test

--- src/components/OpenedCard.js
+++ src/components/OpenedCard.js
@@ -4,8 +4,57 @@ import { useContext } from 'react';
 import { PokemonContext } from '../contexts/PokemonContext';
 import { OpenCardContext } from '../contexts/OpenCardContext';
 import $ from 'jquery';
-import int_to_hashtag from '../tools/int_to_hashtag';
-import ApexCharts from 'apexcharts';
+import int_to_hashtag from '../tools/int_to_hashtag'
+import ApexCharts from 'apexcharts'; 
+import '../css/card.css'
+import fond_image_pokemon from '../images/fond-image-pokemon.svg'
+
+
+
+// images front 
+import fond_card_1 from "../images/fond-card/fond-carte-squircle-1.svg";
+import fond_card_2 from "../images/fond-card/fond-carte-squircle-2.svg";
+import fond_card_3 from "../images/fond-card/fond-carte-squircle-3.svg";
+import fond_card_4 from "../images/fond-card/fond-carte-squircle-4.svg";
+import fond_card_5 from "../images/fond-card/fond-carte-squircle-5.svg";
+import fond_card_6 from "../images/fond-card/fond-carte-squircle-6.svg";
+import fond_card_7 from "../images/fond-card/fond-carte-squircle-7.svg";
+import fond_card_8 from "../images/fond-card/fond-carte-squircle-8.svg";
+import fond_card_9 from "../images/fond-card/fond-carte-squircle-9.svg";
+import fond_card_10 from "../images/fond-card/fond-carte-squircle-10.svg";
+import fond_card_11 from "../images/fond-card/fond-carte-squircle-11.svg";
+import fond_card_12 from "../images/fond-card/fond-carte-squircle-12.svg";
+import fond_card_13 from "../images/fond-card/fond-carte-squircle-13.svg";
+import fond_card_14 from "../images/fond-card/fond-carte-squircle-14.svg";
+import fond_card_15 from "../images/fond-card/fond-carte-squircle-15.svg";
+import fond_card_16 from "../images/fond-card/fond-carte-squircle-16.svg";
+import fond_card_17 from "../images/fond-card/fond-carte-squircle-17.svg";
+import fond_card_18 from "../images/fond-card/fond-carte-squircle-18.svg";
+
+
+// images mask 
+import fond_card_mask_1 from "../images/fond-card-mask/fond-card-mask-1.svg";
+import fond_card_mask_2 from "../images/fond-card-mask/fond-card-mask-2.svg";
+import fond_card_mask_3 from "../images/fond-card-mask/fond-card-mask-3.svg";
+import fond_card_mask_4 from "../images/fond-card-mask/fond-card-mask-4.svg";
+import fond_card_mask_5 from "../images/fond-card-mask/fond-card-mask-5.svg";
+import fond_card_mask_6 from "../images/fond-card-mask/fond-card-mask-6.svg";
+import fond_card_mask_7 from "../images/fond-card-mask/fond-card-mask-7.svg";
+import fond_card_mask_8 from "../images/fond-card-mask/fond-card-mask-8.svg";
+import fond_card_mask_9 from "../images/fond-card-mask/fond-card-mask-9.svg";
+import fond_card_mask_10 from "../images/fond-card-mask/fond-card-mask-10.svg";
+import fond_card_mask_11 from "../images/fond-card-mask/fond-card-mask-11.svg";
+import fond_card_mask_12 from "../images/fond-card-mask/fond-card-mask-12.svg";
+import fond_card_mask_13 from "../images/fond-card-mask/fond-card-mask-13.svg";
+import fond_card_mask_14 from "../images/fond-card-mask/fond-card-mask-14.svg";
+import fond_card_mask_15 from "../images/fond-card-mask/fond-card-mask-15.svg";
+import fond_card_mask_16 from "../images/fond-card-mask/fond-card-mask-16.svg";
+import fond_card_mask_17 from "../images/fond-card-mask/fond-card-mask-17.svg";
+import fond_card_mask_18 from "../images/fond-card-mask/fond-card-mask-18.svg";
+
+
+// images back 
+import back_card from "../images/back-card-squircle.svg";
 
 export default function Opened_card(props) {
   const { typeList } = useContext(PokemonContext);
